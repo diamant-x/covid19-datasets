b005ef742ff61fc4de9d197199012379a470bb18
diamant-x <diamant-x>
2020-03-23 02:40:14 +0100

    Who updates

--- source/data/interim/who_PfdParserToCsvInterim.py
+++ source/data/interim/who_PfdParserToCsvInterim.py
@@ -9,31 +9,61 @@ import csv #To load the tags metadatas
 import glob #To be able to dynamically load files matching pattern.
 import os # To access the OS separator char.
 import tabula # To scan OCR PDFs.
+from ast import literal_eval # To evalaute saved text arrays as real arrays.
 
 #%% Constants Setup
 pathInputFile = "data/raw/who/"
 pathOutputFile = "data/interim/who/"
 metadataFile = "data/raw/who/tabulaParameters.csv"
 
+startFileName = ""
+endFileName = "-covid-19.pdf"
+
 #%% Import metadata to use
-dfMetadataAll = pd.read_csv(metadataFile, sep=";", header=0)
+dfMetadataAll = pd.read_csv(metadataFile, sep=";", header=0, comment='#')
+dfMetadataAll["CoreFileName"] = dfMetadataAll["File"].str.replace(startFileName,"").str.replace(endFileName,"")
+rawFiles = glob.glob(os.path.join(pathInputFile, startFileName+"*"+endFileName))
 
-#%% Process Data Structure N
-dfMetadata = dfMetadataAll[dfMetadataAll["Structure"]=="N"]
-dfMetadata.drop(["Structure"], axis=1, inplace=True)
+#%% Process Data Structure
+for file in rawFiles:
+    fileName = file.split(os.sep)[-1]
+    print("Processing file: " + fileName)
 
-for index, file in dfMetadata.iterrows():
-    print("Processing file: " + file["File"])
-    parsedFile = tabula.read_pdf(pathInputFile+file["File"], lattice = file["lattice"], multiple_tables=file["multiple_tables"], pages=file["pages"], silent = True)
+    dfMetadata = dfMetadataAll[dfMetadataAll["File"]==fileName]
+
+    if dfMetadata.size == 0:
+        #Try to find the previous file metadata identified and apply same settings, if none, then skip file.
+        coreFileName = fileName.replace(startFileName,"").replace(endFileName,"")
+        if coreFileName < dfMetadataAll["CoreFileName"].min():
+            print("Skipping file due to no metadata defined.")
+            continue
+        else:
+            dfMetadata = dfMetadataAll[dfMetadataAll["CoreFileName"]<coreFileName]
+            dfMetadata = dfMetadata[dfMetadata["CoreFileName"]==dfMetadata["CoreFileName"].max()]
+            fileMetadata = dfMetadata.iloc[0]
+    else:
+        fileMetadata = dfMetadata.iloc[0]
+
+    if pd.isnull(fileMetadata["area"]):
+        try:
+            parsedFile = tabula.read_pdf(file, lattice = fileMetadata["lattice"], stream=fileMetadata["stream"], multiple_tables=fileMetadata["multiple_tables"], pages=int(fileMetadata["pages"]), silent = True)
+        except ValueError:
+            parsedFile = tabula.read_pdf(file, lattice = fileMetadata["lattice"], stream=fileMetadata["stream"], multiple_tables=fileMetadata["multiple_tables"], pages=str(fileMetadata["pages"]), silent = True)
+    else:
+        try:
+            parsedFile = tabula.read_pdf(file, lattice = fileMetadata["lattice"], stream=fileMetadata["stream"], relative_area=True, area=literal_eval(fileMetadata["area"]), multiple_tables=fileMetadata["multiple_tables"], pages=int(fileMetadata["pages"]), silent = True)
+        except ValueError:
+            parsedFile = tabula.read_pdf(file, lattice = fileMetadata["lattice"], stream=fileMetadata["stream"], relative_area=True, area=literal_eval(fileMetadata["area"]), multiple_tables=fileMetadata["multiple_tables"], pages=str(fileMetadata["pages"]), silent = True)
+
     if len(parsedFile) > 1:
         tableId = 0
         for table in parsedFile:
             #%% Prepare to write the parsing.
-            outputFilename = file["File"].split(".")[-2] + "-" + str(tableId) + ".csv"
+            outputFilename = fileName.split(".")[-2] + "-" + str(tableId) + ".csv"
             table.to_csv(pathOutputFile+outputFilename, index=False)
             tableId = tableId + 1
     else:
         dfFile = parsedFile[0]
         #%% Prepare to write the parsing.
-        outputFilename = file["File"].split(".")[-2] + ".csv"
+        outputFilename = fileName.split(".")[-2] + ".csv"
         dfFile.to_csv(pathOutputFile+outputFilename, index=False)
---
+++ source/data/processed/who_CleanupAndHomogenization.py
@@ -0,0 +1,130 @@
+#%% [markdown]
+# # CSV Consolidation ESP
+# Creation date: 2020-03-20
+
+#%% Imports
+import pandas as pd # To work similar to R in Python
+import numpy # To access pi number definition.
+import csv #To load the tags metadatas
+import os # To access the OS separator char.
+import glob #To be able to dynamically load files matching pattern.
+import io # To work with files read/write 
+import re # To replace using regex rules for complex scenarios
+import _csv
+
+#%% Constants Setup
+pathInputFile = "data/interim/who/"
+pathOutputFile = "data/processed/who/"
+metadataFile = "data/raw/who/tabulaParameters.csv"
+
+startFileName = "20"
+endFileName = "-covid-19.csv"
+
+
+namesOfMultilineRegions2 = {"Iran (Islamic,":"Iran (Islamic Republic of),", "Iran (Islamic Republic,":"Iran (Islamic Republic of),", "United Arab,":"United Arab Emirates,", "United States Virgin,":"United States Virgin Islands", "Democratic Republic,":"Democratic Republic of the Congo", "United Republic of,":"United Republic of Tanzania", "Central African,":"Central African Republic", "Subtotal for all,":"Total", "International,":"Diamond Princess", "Bosnia and,":"Bosnia and Herzegovina"}
+
+namesOfMultilineRegions = ["Bosnia and", "Iran (Islamic", "United Arab", "Occupied", "occupied Palestinian", "United States of", "Venezuela (Bolivarian", "Bolivia (Plurinational", "Saint Vincent and the"]
+
+namesOfDroppedRows = ["Total", "Unnamed", "Country", "Territory/Area", "deaths", "Western Pacific Region", "erritories**", "European Region", "outh-East Asia Region", "Mediterranean Region", "egion of the Americas", "frican Region", "Subtotal", "Grand total", "regions,,,", "conveyance,,", "Diamond Princess", "Islands,,,", "of the Congo,,,", "Tanzania,,,", "Republic,,,", "Republic of),,,", "Emirates,,,", "regions"]
+
+
+#%% Import metadata to use
+dfMetadataAll = pd.read_csv(metadataFile, sep=";", header=0, comment="#")
+dfMetadataAll["ID"] = dfMetadataAll["File"].str.replace(startFileName,"").str.replace(endFileName.replace("csv","pdf"),"")
+
+rawFiles = glob.glob(os.path.join(pathInputFile, startFileName+"*"+endFileName))
+
+#%% Iterate through files.
+for file in rawFiles:
+    fileName = file.split(os.sep)[-1]
+    print("Processing file: " + fileName)
+
+    with io.open(pathOutputFile+fileName, mode='w', encoding="utf-8") as outputFileObject:
+        with io.open(file, mode='r', encoding="utf-8") as fileObject:
+            prependNextLine = ""
+            for line in fileObject:
+                if any(region in line for region in list(namesOfMultilineRegions2)):
+                    for key, value in namesOfMultilineRegions2.items():
+                        if key in line:
+                            prependNextLine = value
+                    continue
+                elif any(region in line for region in namesOfMultilineRegions):
+                    prependNextLine = line.replace(',', '').replace('\n', ' ').replace('\r', '')
+                    continue
+                elif any(region in line for region in namesOfDroppedRows):
+                    continue
+                else:
+                    if "Total" in prependNextLine:
+                        pass
+                    else:
+                        line = prependNextLine+line.replace("conveyance","").replace("§","").replace(".",",").replace("†","")
+                        line = re.sub(r'([a-zA-Z])\s+([0-9])', r'\1,\2', line)
+                        line = re.sub(r',,,+', r',', line)
+                        outputFileObject.write( line )
+
+                    prependNextLine = ""
+        
+    continue
+    # Date will be extracted from filename
+    date = pd.to_datetime(fileName.replace(startFileName,"").replace(endFileName,""))
+
+    # Column Structure will be calculated sequentially based on the previous highest id's date
+    fileId = fileName.replace(startFileName,"").replace(endFileName,"")
+    dfMetadataTemp = dfMetadataAll[dfMetadataAll["ID"]<=fileId]
+    dfMetadataTemp = dfMetadataTemp[dfMetadataTemp["ID"]==(dfMetadataTemp["ID"].max())].iloc[0]
+    fileStructureId = dfMetadataTemp["Structure"]
+
+    if fileStructureId == 5:
+        namesColumns = ["Region", "Total confirmed cases", "New cases", "Population Incidence Ratio", "Total deaths"]
+
+    try:
+        dfImported = pd.read_csv(pathOutputFile+fileName, skipinitialspace=True, header=None, skipfooter=0, encoding='utf-8', engine="python", index_col=False)
+    except:
+        try:
+            dfImported = pd.read_csv(pathOutputFile+fileName, sep=";", skipinitialspace=True, header=None, skipfooter=0, encoding='utf-8', engine="python", index_col=False)
+        except:
+            dfImported = pd.read_csv(pathOutputFile+fileName, sep=" ", skipinitialspace=True, header=None, skipfooter=0, encoding='utf-8', engine="python", index_col=False)
+
+    if len(dfImported.columns) == 1:
+        dfImported = pd.read_csv(pathOutputFile+fileName, sep=";", skipinitialspace=True, header=None, encoding='utf-8', engine="python", index_col=False)
+
+    # Find the columns where each value is null. Source: https://www.jitsejan.com/find-and-delete-empty-columns-pandas-dataframe.html
+    empty_cols = [col for col in dfImported.columns if dfImported[col].isnull().all()]
+    # Drop these columns from the dataframe
+    dfImported.drop(empty_cols, axis=1, inplace=True)
+
+    dfImported.rename(columns=dict(zip(dfImported.columns,namesColumns)), inplace=True)
+    
+    try:
+        dfImported["Total confirmed cases"] = dfImported["Total confirmed cases"].str.replace(",","").astype('float64')
+        dfImported.loc[dfImported["Total confirmed cases"].round() != dfImported["Total confirmed cases"], "Total confirmed cases"] = dfImported["Total confirmed cases"]*1000
+    except AttributeError:
+        pass
+
+    try:
+        dfImported.insert(2, "First", dfImported["New cases"].str.split(" ", expand=True)[0], allow_duplicates=False)
+        dfImported.insert(3, "Last", dfImported["New cases"].str.split(" ", expand=True)[1], allow_duplicates=False)
+  
+        dfImported.drop("New cases", axis=1, inplace=True)
+    except AttributeError:
+        pass
+
+    dfImported.fillna(0, inplace=True)
+
+    dfImported.rename(columns=dict(zip(dfImported.columns,namesColumns)), inplace=True)
+
+    dfImported.insert(0, "Date", date.date(), allow_duplicates=False) 
+    dfImported["Date"] = dfImported["Date"].astype(str)
+    dfImported["Region"] = dfImported["Region"].astype(str)
+    dfImported["Total confirmed cases"] = dfImported["Total confirmed cases"].astype('int64')
+    dfImported["New cases"] = dfImported["New cases"].astype('int64')
+    dfImported["Population Incidence Ratio"] = dfImported["Population Incidence Ratio"].astype('float64')
+    dfImported["Total deaths"] = dfImported["Total deaths"].astype('int64')
+
+    #%% Write to file cleaned dataframe
+    dfImported.to_csv(path_or_buf=pathOutputFile+fileName, index=False, quoting=csv.QUOTE_NONNUMERIC)
+
+print("Done.")
+
+
+# %%
