39876d37e6c322e5fb214c11f060d44988452d59
diamant-x <diamant-x>
2020-03-28 17:27:23 +0100

    Updated to clean strange raw symbols.

--- source/data/processed/esp_CleanupAndHomogenization.py
+++ source/data/processed/esp_CleanupAndHomogenization.py
@@ -43,7 +43,7 @@ for file in rawFiles:
             for line in fileObject:
                 if any(region in line for region in namesOfRegions):
                     for region in namesOfRegions:
-                        line = line.replace(region, region+",")
+                        line = line.replace(region, region+",").replace("¥", "")
                     outputFileObject.write(line)
                 else:
                     continue
